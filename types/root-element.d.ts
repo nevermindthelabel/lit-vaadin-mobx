89b6cc002bb81c12c1fece092fcd0c3cb602a609
Matt <nevermindthelabel@gmail.com>
2022-06-03 07:40:41 -0700

    types

--- types/root-element.d.ts
+++ types/root-element.d.ts
@@ -1,11 +1,7 @@
 import { LitElement } from 'lit';
 import './views/todo-view';
-/**
- * An example element.
- *
- * @slot - This element has a slot
- * @csspart button - The button
- */
+import './components/header';
+import './components/footer';
 export declare class RootElement extends LitElement {
     static styles: import("lit").CSSResult;
     render(): import("lit-html").TemplateResult<1>;
