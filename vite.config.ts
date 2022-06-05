ab59afce32f1ab8d57fb16244875005229a87634
Matt <nevermindthelabel@gmail.com>
2022-06-04 17:28:48 -0700

    updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/my-element.ts',
+      entry: 'src/root-element.ts',
       formats: ['es']
     },
     rollupOptions: {
       external: /^lit/
     }
   }
-})
+});
